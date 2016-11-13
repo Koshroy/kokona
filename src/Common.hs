f4f925677113e46c9f65f8daa47f7ca143505789
Koushik Roy <koushik@meff.me>
2016-11-13 00:26:24 +0000

    checkpoint

--- src/Common.hs
+++ src/Common.hs
@@ -3,12 +3,17 @@
 
 module Common
     ( SlackMessage(..)
+    , MessageAcceptor
 
     , slackPayloadWithChannel
+    , startMessageAcceptor
+    , textInMessageAcceptor
     ) where
 
 import ClassyPrelude
 import Data.Aeson
+
+type MessageAcceptor = Text -> Bool
 
 
 data SlackMessage = SlackMessage
@@ -16,11 +21,33 @@ data SlackMessage = SlackMessage
   , message :: Text
   } deriving (Eq, Show)
 
+
 instance ToJSON SlackMessage where
   toJSON sm = object [
     "channel" .= channel sm,
     "text" .= message sm ]
 
+
 slackPayloadWithChannel :: Text -> Text -> Text
 slackPayloadWithChannel channel payload =
   decodeUtf8 $ toStrict $ encode $ SlackMessage channel payload
+
+
+startMessageAcceptorGenerator :: Text -> Text -> Bool
+startMessageAcceptorGenerator start message =
+  let messageWords = words message in
+    case (headMay messageWords) of
+      Nothing -> False
+      Just firstWord -> firstWord == start
+
+
+startMessageAcceptor :: Text -> MessageAcceptor
+startMessageAcceptor start = startMessageAcceptorGenerator start
+
+textInMessageAcceptorGenerator :: Text -> Text -> Bool
+textInMessageAcceptorGenerator needle message =
+  isInfixOf needle message
+
+textInMessageAcceptor :: Text -> MessageAcceptor
+textInMessageAcceptor needle =
+  textInMessageAcceptorGenerator needle
